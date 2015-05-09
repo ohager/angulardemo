add271c0a8da7862e0a00780ad51d71f7d58c928
ohager <oliver.hager@dextra.com.br>
2015-05-09 01:15:02 -0300

    initial commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'ui.bootstrap',
+  'myApp.itemlist',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/itemlist'});
+}]);
---
+++ app/itemlist/itemlist.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular.module('myApp.itemlist', ['ngRoute'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/itemlist', {
+            templateUrl: 'itemlist/itemlist.html',
+            controller: 'ItemListController'
+        });
+    }])
+
+    .controller('ItemListController', ['$scope', '$interval', function ($scope, $interval) {
+
+
+        var Player = new function() {
+            var handle = null;
+
+            this.start = function( func, millies){
+                handle = $interval( func, millies);
+            };
+
+            this.stop = function(){
+                $interval.cancel(handle);
+            };
+        };
+
+        var lastTime = 0;
+
+        function reseed(itemCount) {
+            var data = [];
+
+            var now = performance.now();
+            $scope.frameMs = now - lastTime;
+            lastTime = now;
+            for (var i = 0; i < itemCount; ++i) {
+                data.push(
+                    {
+                        id:Math.floor((Math.random() * itemCount)),
+                        timestamp: Date.now()
+                    })
+            }
+
+            return data;
+        }
+
+        function chunk(arr, size) {
+            var newArr = [];
+            for (var i = 0; i < arr.length; i += size) {
+                newArr.push(arr.slice(i, i + size));
+            }
+            return newArr;
+        }
+
+        function shuffle(){
+            var data = reseed($scope.items.count);
+            $scope.items = {
+                count : data.length,
+                rows : chunk(data,3)
+            }
+        }
+
+        $scope.togglePause = function(){
+            $scope.control.isPaused = !$scope.control.isPaused;
+            if($scope.control.isPaused){
+                Player.stop();
+            }else{
+                Player.start(shuffle, $scope.control.interval);
+            }
+        };
+
+        $scope.control = {
+            interval : 500,
+            isPaused : true
+        };
+
+
+        $scope.items ={
+            count : 100,
+            rows : []
+        };
+        shuffle();
+
+    }]);
